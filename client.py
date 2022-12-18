1c4d27481523844f742f0e356d643255195ca449
Iaroslav87 <91834197+Iaroslav87@users.noreply.github.com>
2022-12-18 18:18:09 +0300

    Lesson8
    HW

--- client.py
+++ client.py
@@ -1,28 +1,79 @@
-import chat
-import jim
+import sys
+import time
+import logging
+import multiprocessing
+import select
+import chat.chat as chat
+import chat.jim as jim
+import log.client_log_config
+
+logger = logging.getLogger('chat.client')
+
+
+def send(sock):
+    while True:
+        msg = input('Введите сообщение ("exit" для выхода): ')
+
+        if msg:
+            print(msg)
+            jim.MESSAGE['message'] = msg
+
+            try:
+                chat.send_data(sock, jim.MESSAGE)
+            except ConnectionResetError as e:
+                logger.error(e)
+                break
+
+
+def receive(sock):
+    while True:
+        try:
+            data = chat.get_data(sock)
+        except ConnectionResetError as e:
+            logger.error(e)
+            break
+
+        if data['response'] != '200':
+            logger.debug('App ending')
+            break
+
+        if 'messages' in data:
+            for message in data['messages']:
+                sys.stdout.write(
+                    f'{message["time"]} - {message["from"]}: {message["message"]}')
+
 
 if __name__ == '__main__':
-    client_name = input('Введите имя: ')
+    logger.debug('App started')
 
     parser = chat.create_parser()
     namespace = parser.parse_args()
 
-    sock = chat.get_client_socket(namespace.addr, namespace.port)
+    client_name = input('Введите имя: ')
 
+    sock = chat.get_client_socket(namespace.addr, namespace.port)
     serv_addr = sock.getpeername()
-    print(f'Connected to server: {serv_addr[0]}:{serv_addr[1]}')
+    start_info = f'Connected to server: {serv_addr[0]}:{serv_addr[1]}'
+    print(start_info)
+    logger.info(start_info)
 
     jim.PRESENCE['user']['account_name'] = client_name
-    chat.send_data(sock, jim.PRESENCE)
+    try:
+        chat.send_data(sock, jim.PRESENCE)
+    except ConnectionResetError as e:
+        logger.error(e)
+        sock.close()
+        exit(1)
 
-    while True:
-        data = chat.get_data(sock)
+    p_send = multiprocessing.Process(target=send, args=(sock,))
+    p_receive = multiprocessing.Process(target=receive, args=(sock,))
 
-        if data['response'] != '200':
-            break
+    p_send.start()
+    p_receive.start()
 
-        msg = input('Введите сообщение ("exit" для выхода): ')
-        jim.MESSAGE['message'] = msg
-        chat.send_data(sock, jim.MESSAGE)
+    if not p_send.is_alive() or not p_receive.is_alive():
+        exit(1)
 
+    p_send.join()
+    p_receive.join()
     sock.close()
